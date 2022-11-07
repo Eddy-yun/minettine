60317847fa6dec7a21d9ddfeb360df28e05560dc
eddy <kimtaeyun1@naver.com>
2022-11-07 23:27:15 +0900

    project setting commit
    1. project setting commit

--- src/main/java/com/minettine/web/main/MainController.java
+++ src/main/java/com/minettine/web/main/MainController.java
@@ -1,2 +1,14 @@
-package com.minettine.web.main;public class MainController {
+package com.minettine.web.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping( "/hello" )
+    public String hello() {
+        return "hello";
+    }
+
 }
